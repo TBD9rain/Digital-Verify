5c186d6c20727a91a244d9fbd1aab0e45f5d7bf9
lshi1 <lingwei.shi@latticesemi.com>
2025-09-16 16:16:14 +0800

    refactor: main phase task definition codes in scoreboard

--- example/uvm/tb/adder/class/Scb.sv
+++ example/uvm/tb/adder/class/Scb.sv
@@ -1,7 +1,7 @@
 //==================================================================================================
 //
 //  Project         :   Digital Verify Example
-//  Version         :   v1.1.0
+//  Version         :   v1.1.1
 //  Title           :   Scb
 //
 //  Description     :   scoreboard class definition
@@ -38,50 +38,41 @@ class Scb #(
     endfunction
 
     task main_phase(uvm_phase phase);
+        ITXN itxn_get;
+        OTXN otxn_get;
+
         ITXN itxn;
         OTXN otxn;
-        ITXN imon_txn;
-        OTXN mdl_txn;
-        OTXN omon_txn;
-        bit txn_equal;
+
+        ITXN stimulus_input;
+        OTXN expected_output;
+        OTXN actual_output;
 
         fork
             forever begin
-                imon_getp.get(itxn);
-                imon_txn_q.push_front(itxn);
+                imon_getp.get(itxn_get);
+                imon_txn_q.push_front(itxn_get);
             end
             forever begin
-                mdl_getp.get(otxn);
-                mdl_txn_q.push_front(otxn);
+                mdl_getp.get(otxn_get);
+                mdl_txn_q.push_front(otxn_get);
             end
             forever begin
-                omon_getp.get(omon_txn);
+                actual_output = OTXN::type_id::create("actual_output");
+                omon_getp.get(otxn);
+                actual_output.copy(otxn);
+
                 if (imon_txn_q.size() > 0 && mdl_txn_q.size() > 0) begin
-                    //  value check
-                    txn_equal = omon_txn.compare(mdl_txn);
-                    mdl_txn = mdl_txn_q.pop_back();
-                    if (txn_equal) begin
-                        `uvm_info("Scb", "expected output and actual output match.", UVM_MEDIUM)
-                    end
-                    else begin
-                        `uvm_error("Scb", "expected output and actual output mismatch.")
-                        `uvm_info("Scb", "expected output:", UVM_NONE)
-                        mdl_txn.print();
-                        `uvm_info("Scb", "actual output:", UVM_NONE)
-                        omon_txn.print();
-                    end
-                    //  latency check
-                    imon_txn = imon_txn_q.pop_back();
-                    if (omon_txn.timestamp - imon_txn.timestamp === LATENCY) begin
-                        `uvm_info("Scb", "DUT latency is as expected.", UVM_MEDIUM)
-                    end
-                    else begin
-                        `uvm_error("Scb", "DUT latency is not as expected.")
-                        `uvm_info("Scb", "input monitor transaction:", UVM_NONE)
-                        imon_txn.print();
-                        `uvm_info("Scb", "output monitor transaction:", UVM_NONE)
-                        omon_txn.print();
-                    end
+                    expected_output = OTXN::type_id::create("expected_output");
+                    otxn = mdl_txn_q.pop_back();
+                    expected_output.copy(otxn);
+
+                    stimulus_input = ITXN::type_id::create("stimulus_input");
+                    itxn = imon_txn_q.pop_back();
+                    stimulus_input.copy(itxn);
+
+                    value_check(expected_output, actual_output);
+                    latency_check(stimulus_input, actual_output);
                 end
                 else begin
                     `uvm_error("Scb", "unexpected DUT output with no input.")
@@ -89,6 +80,36 @@ class Scb #(
             end
         join
     endtask
+
+    function void value_check(const ref OTXN exp_txn, const ref OTXN act_txn);
+        bit txn_equal;
+
+        txn_equal = exp_txn.compare(act_txn);
+        if (txn_equal) begin
+            `uvm_info("Scb", "expected output and actual output match.", UVM_MEDIUM)
+        end
+        else begin
+            `uvm_error("Scb", "expected output and actual output mismatch.")
+            `uvm_info("Scb", "expected output:", UVM_NONE)
+            exp_txn.print();
+            `uvm_info("Scb", "actual output:", UVM_NONE)
+            act_txn.print();
+        end
+    endfunction
+
+    function void latency_check(const ref ITXN stm_txn, const ref OTXN act_txn);
+        longint unsigned dut_latency;
+
+        dut_latency = act_txn.timestamp - stm_txn.timestamp;
+        if (dut_latency == LATENCY) begin
+            `uvm_info("Scb", "DUT latency is as expected.", UVM_MEDIUM)
+        end
+        else begin
+            `uvm_error("Scb", "DUT latency is not as expected.")
+            `uvm_info("Scb", $sformatf("expected latency is %0d clocks.", LATENCY), UVM_NONE)
+            `uvm_info("Scb", $sformatf("actual latency is %0d clocks.", dut_latency), UVM_NONE)
+        end
+    endfunction
 endclass
 
 
